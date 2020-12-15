1ff36fe8946a31805e6d864018fed0c2a5656531
Tammy Tran <tammy.n.tran@gmail.com>
2020-12-15 20:12:26 +0000

    updated counter to turn red when over 140 characteros are in the textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,6 +4,11 @@ $(document).ready(function () {
     let charCount = $(this).val().length;
     let newCount = 140 - charCount;
     $(this).siblings().children('.counter').val(newCount);
+    if (newCount < 0) {
+      $(this).siblings().children('.counter').addClass('negative')
+    } else {
+      $(this).siblings().children('.counter').removeClass('negative')
+    }
   })
 
 });
