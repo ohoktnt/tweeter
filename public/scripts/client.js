80f98301d836eac5c7a694455c4618457c85d8ab
Tammy Tran <tammy.n.tran@gmail.com>
2020-12-17 23:14:28 +0000

    cleaned up code: comments and organized

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,7 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 */
 
-
 $(document).ready(function () {
   
   // Cross-Site Scripting Prevention
@@ -13,80 +12,80 @@ $(document).ready(function () {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   }
-
-  renderTweets = function(tweetArr) {
-    // to eliminate duplicates
-    $('#tweets-container').empty()
-    // loops through tweets
-    for(let tweet of tweetArr) {
-      console.log(tweet)
-      // calls createtweet elemnt for each tweet
-      const $tweet = createTweetElement(tweet);
-      // takes return value and appends it to the tweets container
-      $('#tweets-container').prepend($tweet);
-    }
-  }
-
-  // toggle new tweet button
+  
+  // toggle new tweet button on nav
   $('.newtweet-group').on('click', function() {
     $('.new-tweet').slideToggle('slow')
     $('#new-tweet-form').children('textarea').focus();
   })
-  
-  createTweetElement = function(tweetObj) {
-    const time = createTimeAgo(tweetObj.created_at);
-    const $tweet = $(`        
-    <article class="tweet">
-    <header>
-      <div>
-        <h3><img src="${tweetObj.user.avatars}"> &ensp; ${tweetObj.user.name}</h3>
-        <h3 class="userID">${tweetObj.user.handle}</h3>
-      </div>
-    </header>
-    <p>${escape(tweetObj.content.text)}</p>
-    <footer>
-      <div>${time}</div>
-      <div class="icons"><i class="fas fa-flag"></i>  <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></div>
-    </footer>
-  </article>`);
-    return $tweet;
-  }
 
+  // date helper function 
   createTimeAgo = function(milliseconds) {
     let now = Date.now()
     const timePassed = now - milliseconds;
-    // 1 day ago
+    // Time conditions 
     const daysPassed = Math.floor(timePassed/86400000)
+    // 1 day ago
     if(daysPassed === 1) {
       return `1 day ago`
     }
-    // hours ago
     if (daysPassed < 1) {
       const hoursPassed = Math.floor(timePassed/3600000);
-
-    // mins ago
       if (hoursPassed < 1) {
         const minsPassed = Math.floor(timePassed/60000)
+        // mins ago
         return `${minsPassed} mins ago`
       }
+      // hours ago
       return `${hoursPassed} hours ago`
     }
     // days ago
     return `${daysPassed} days ago`;
   }
 
-  // adding New Tweets
-  // will collect information from form
+  // Tweet management // 
+
+  renderTweets = function(tweetArr) {
+    // to eliminate duplicates
+    $('#tweets-container').empty()
+    // loops through tweets data 
+    for(let tweet of tweetArr) {
+      // calls createtweet element for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container in order of newest first
+      $('#tweets-container').prepend($tweet);
+    }
+  }
+  
+  // new tweet HTML with data filled
+  createTweetElement = function(tweetObj) {
+    const time = createTimeAgo(tweetObj.created_at);
+    const $tweet = $(`        
+    <article class="tweet">
+    <header>
+    <div>
+    <h3><img src="${tweetObj.user.avatars}"> &ensp; ${tweetObj.user.name}</h3>
+    <h3 class="userID">${tweetObj.user.handle}</h3>
+    </div>
+    </header>
+    <p>${escape(tweetObj.content.text)}</p>
+    <footer>
+    <div>${time}</div>
+    <div class="icons"><i class="fas fa-flag"></i>  <i class="fas fa-retweet"></i> <i class="fas fa-heart"></i></div>
+    </footer>
+    </article>`);
+    return $tweet;
+  }
+  
+  // adding New Tweet
   $('#new-tweet-form').on("submit", function(event) {
     // stop the form from being submitted
     event.preventDefault();
-    console.log('submit default stopped')
     
+    // new tweet text 
     const $tweet = $(this).children('textarea').serialize();
-    // const tweetText = $tweet.val();
-    console.log($tweet)
-    
-    // form validation
+
+    // form validation for new tweet text
     $('#error').empty();
     const counter = Number($(this).children('div').children('output').val())
     // console.log(counter);
@@ -98,42 +97,40 @@ $(document).ready(function () {
       return $('#error').append(message);
     } 
     
-
     // Create AJAX request - for new tweet
     $.ajax({
       method: 'POST',
       url: 'http://localhost:8080/tweets',
       data: $tweet
     })
-      .done(function () {
-        console.log('ajax data sent?')
-        loadTweets();
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-
-    $(this).children('textarea').val('');
-    $(this).children('div').children('output').val('140');
+    .done(function () {
+      loadTweets();
+      // clear out counter and text area on submission 
+      $(this).children('textarea').val('');
+      $(this).children('div').children('output').val('140');
+    })
+    .catch((err) => {
+      console.log(err)
+    })
   })
   
   loadTweets = function() {
-    // make request to /tweets and recieve array of tweets as JSON
+    // make request to /tweets and receive array of tweets as JSON
     $.ajax({
       method: 'GET',
       url: 'http://localhost:8080/tweets'
     })
-    // on sucess, call the renderTWeets function passing the response
-      .then((result) => {
-        console.log(result)
-        renderTweets(result)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-
+    // on successful, call the renderTWeets function passing the response
+    .then((result) => {
+      console.log(result)
+      renderTweets(result)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    
   }
-
+  
   loadTweets();
   
 })
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
   
+  // function to turn counter red when user passed character limit
   $('#tweet-text').on('input', function() {
     let charCount = $(this).val().length;
     let newCount = 140 - charCount;
@@ -10,4 +11,5 @@ $(document).ready(function () {
       $(this).siblings().children('.counter').removeClass('negative')
     }
   })
+
 });
